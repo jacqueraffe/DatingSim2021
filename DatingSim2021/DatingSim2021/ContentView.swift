abeb297c4a48d89dec22c693a91476bcd5e223f3
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2021-09-25 15:20:04 -0700

    Made Xcode project.

---
+++ DatingSim2021/DatingSim2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DatingSim2021
+//
+//  Created by Jacqueline Palevich on 9/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DatingSim2021/DatingSim2021/DatingSim2021App.swift
@@ -0,0 +1,17 @@
+//
+//  DatingSim2021App.swift
+//  DatingSim2021
+//
+//  Created by Jacqueline Palevich on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct DatingSim2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
