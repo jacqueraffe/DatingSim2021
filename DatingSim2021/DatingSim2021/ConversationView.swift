ea734fac772a8ec32358b47a635a257533f1ac6d
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2021-09-27 10:45:10 -0700

    got chapter 2 done, also made ui better, with boxes n stuff

--- DatingSim2021/DatingSim2021/ConversationView.swift
+++ DatingSim2021/DatingSim2021/ConversationView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ConversationView: View {
-    @StateObject private var conversationModel = ConversationModel(conversationName: "Chapter1")
+    
+    @StateObject private var conversationModel = ConversationModel(conversationName: "Chapter2")
     
     var body: some View {
         let node = conversationModel.currentNode
         Text(node.prompt)
+            .fontWeight(.bold)
+                .font(.title)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(40)
+                .foregroundColor(.white)
+                .padding(10)
         let choices = node.choices
         ForEach(node.choices) { choice in
             Button(choice.label){
@@ -20,6 +28,10 @@ struct ConversationView: View {
                 let offset = choices.firstIndex{$0.label == choice.label}!
                 conversationModel.pick(choice: offset)
             }
+            .padding()
+            .background(Color.purple)
+            .foregroundColor(.white)
+            .font(.title)
         }
     }
 }
